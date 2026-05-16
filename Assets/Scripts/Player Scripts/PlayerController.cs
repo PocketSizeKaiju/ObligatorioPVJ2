7ce320d484e7760b598c4934fd3d3d01d0ecab6a
PocketSizeKaiju <157993298+PocketSizeKaiju@users.noreply.github.com>
2026-05-16 16:49:21 -0300

    Added tilemap configuration

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -8,41 +8,36 @@ public class PlayerMovement : MonoBehaviour
 
     private float Speed => Settings.Instance.PlayerSpeed;
 
-    /* // #region  Player bounds
-    private float PlayerBoundsYMin => Settings.Instance.PlayerBoundsYMin;
-    private float PlayerBoundsYMax => Settings.Instance.PlayerBoundsYMax;
-    private float PlayerBoundsXMin => Settings.Instance.PlayerBoundsXMin;
-    private float PlayerBoundsXMax => Settings.Instance.PlayerBoundsXMax;
-    // #endregion */
-
     private Vector2 _lastNonZeroDirection;
+    private Rigidbody2D _rigidBody;
 
-    void Update()
+    private Vector2 _movement;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
     {
         UpdateKeyboardInput();
     }
 
+    private void FixedUpdate()
+    {
+        _rigidBody.linearVelocity = _movement.normalized * Speed;
+    }
+
     private void UpdateKeyboardInput()
     {
-        Vector2 direction = new Vector2(
+        _movement = new Vector2(
             ((Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed) ? 1 : 0) -
             ((Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed) ? 1 : 0),
             ((Keyboard.current.upArrowKey.isPressed || Keyboard.current.wKey.isPressed) ? 1 : 0) -
             ((Keyboard.current.downArrowKey.isPressed || Keyboard.current.sKey.isPressed) ? 1 : 0)
         );
 
-        Vector3 velocity = (Vector3)(direction.normalized * Speed);
-        Vector3 pos = transform.position;
-
-        pos += velocity * Time.deltaTime;
-        /* pos.y = Mathf.Clamp(pos.y, PlayerBoundsYMin, PlayerBoundsYMax);
-        pos.x = Mathf.Clamp(pos.x, PlayerBoundsXMin, PlayerBoundsXMax); */
-
-        transform.position = pos;
-
-        if (direction != Vector2.zero)
-        {
-            _lastNonZeroDirection = direction;
-        }
+        if (_movement != Vector2.zero)
+            _lastNonZeroDirection = _movement;
     }
 }
