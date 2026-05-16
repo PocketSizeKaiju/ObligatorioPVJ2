4cabe7c93f712209f2f9e7573333951f8eabbfa9
PocketSizeKaiju <157993298+PocketSizeKaiju@users.noreply.github.com>
2026-05-16 14:52:39 -0300

    Camera follows player

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -2,55 +2,47 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    public Vector2 LastNonZeroDirection => _lastNonZeroDirection;
+
     private float Speed => Settings.Instance.PlayerSpeed;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    /* // #region  Player bounds
+    private float PlayerBoundsYMin => Settings.Instance.PlayerBoundsYMin;
+    private float PlayerBoundsYMax => Settings.Instance.PlayerBoundsYMax;
+    private float PlayerBoundsXMin => Settings.Instance.PlayerBoundsXMin;
+    private float PlayerBoundsXMax => Settings.Instance.PlayerBoundsXMax;
+    // #endregion */
 
-    // Update is called once per frame
+    private Vector2 _lastNonZeroDirection;
+
     void Update()
     {
         UpdateKeyboardInput();
-        UpdateMouseInput();
-    }
-
-    private void UpdateMouseInput()
-    {
-        if (Mouse.current.leftButton.wasPressedThisFrame)
-        {
-            Debug.Log("click");
-        }
     }
 
     private void UpdateKeyboardInput()
     {
+        Vector2 direction = new Vector2(
+            ((Keyboard.current.rightArrowKey.isPressed || Keyboard.current.dKey.isPressed) ? 1 : 0) -
+            ((Keyboard.current.leftArrowKey.isPressed || Keyboard.current.aKey.isPressed) ? 1 : 0),
+            ((Keyboard.current.upArrowKey.isPressed || Keyboard.current.wKey.isPressed) ? 1 : 0) -
+            ((Keyboard.current.downArrowKey.isPressed || Keyboard.current.sKey.isPressed) ? 1 : 0)
+        );
+
+        Vector3 velocity = (Vector3)(direction.normalized * Speed);
         Vector3 pos = transform.position;
-        
-        Vector3 direction = Vector2.zero;
 
-        if (Keyboard.current.leftArrowKey.IsPressed())
-        {
-            direction.x += -1;
-        }
-        if (Keyboard.current.rightArrowKey.IsPressed())
-        {
-            direction.x += 1;
-        }
-        if (Keyboard.current.downArrowKey.IsPressed())
-        {
-           direction.y += -1;
-        }
-        if (Keyboard.current.upArrowKey.IsPressed())
-        {
-            direction.y += 1;
-        }
+        pos += velocity * Time.deltaTime;
+        /* pos.y = Mathf.Clamp(pos.y, PlayerBoundsYMin, PlayerBoundsYMax);
+        pos.x = Mathf.Clamp(pos.x, PlayerBoundsXMin, PlayerBoundsXMax); */
 
-        Vector3 velocity = direction.normalized * Speed;
-        transform.position += velocity * Time.deltaTime;
+        transform.position = pos;
+
+        if (direction != Vector2.zero)
+        {
+            _lastNonZeroDirection = direction;
+        }
     }
 }
